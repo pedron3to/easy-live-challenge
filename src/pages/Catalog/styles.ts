c61df9dbbb3202ef45a579758bfb78379d87b0a9
pedron3to <62304426+pedron3to@users.noreply.github.com>
2021-07-19 14:58:22 -0300

    added margin-bottom to catalog page

--- src/pages/Catalog/styles.ts
+++ src/pages/Catalog/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 70px;
+  padding: 70px 0;
 `;
 
 export const Title = styled.div`
